563e5fd80574f232e54a6cc13509c818a39364ab
liuzhenghe30265 <15901450207@163.com>
2021-11-27 10:37:27 +0800

    fix:update

--- src/App.vue
+++ src/App.vue
@@ -17,6 +17,60 @@ export default {
       infoBox: false
     })
     console.log(viewer)
+
+    // 事件
+    let handler = new Cesium.ScreenSpaceEventHandler(viewer.scene.canvas)
+    handler.setInputAction(function (event) {
+      console.log('左键单击事件：', event, event.position)
+    }, Cesium.ScreenSpaceEventType.LEFT_CLICK)
+
+    handler.setInputAction(function (event) {
+      console.log('左键双击事件：', event)
+    }, Cesium.ScreenSpaceEventType.LEFT_DOUBLE_CLICK)
+
+    handler.setInputAction(function (event) {
+      console.log('左键按下事件：', event)
+    }, Cesium.ScreenSpaceEventType.LEFT_DOWN)
+
+    handler.setInputAction(function (event) {
+      console.log('左键弹起事件：', event)
+    }, Cesium.ScreenSpaceEventType.LEFT_UP)
+
+    handler.setInputAction(function (event) {
+      console.log('中键单击事​​件：', event)
+    }, Cesium.ScreenSpaceEventType.MIDDLE_CLICK)
+
+    handler.setInputAction(function (event) {
+      console.log('中键按下事件：', event)
+    }, Cesium.ScreenSpaceEventType.MIDDLE_DOWN)
+
+    handler.setInputAction(function (event) {
+      console.log('中键弹起事件：', event)
+    }, Cesium.ScreenSpaceEventType.MIDDLE_UP)
+
+    handler.setInputAction(function (movement) {
+      console.log('移动事件：', movement, movement.endPosition)
+    }, Cesium.ScreenSpaceEventType.MOUSE_MOVE)
+
+    handler.setInputAction(function (click) {
+      console.log('右键单击事件', click.position)
+    }, Cesium.ScreenSpaceEventType.RIGHT_CLICK)
+
+    handler.setInputAction(function (event) {
+      console.log('右键按下事件：', event)
+    }, Cesium.ScreenSpaceEventType.RIGHT_DOWN)
+
+    handler.setInputAction(function (event) {
+      console.log('右键弹起事件：', event)
+    }, Cesium.ScreenSpaceEventType.RIGHT_UP)
+
+    handler.setInputAction(function (wheelment) {
+      console.log('滚轮事件：', wheelment)
+    }, Cesium.ScreenSpaceEventType.WHEEL)
+
+    // 删除事件
+    handler.removeInputAction(Cesium.ScreenSpaceEventType.WHEEL)
+
   }
 }
 </script>
